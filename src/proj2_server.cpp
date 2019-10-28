24dd126b4267cf3095c670ccea2040143ea3841a
marcin <mahanas@outlook.com>
2019-10-28 22:26:21 +0100

    Global costmap and planner working

--- src/proj2_server.cpp
+++ src/proj2_server.cpp
@@ -5,6 +5,15 @@
 #include <string>
 #include "costmap_2d/costmap_2d_ros.h"
 #include "tf2_ros/buffer.h"
+#include <tf2_ros/static_transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <cstdio>
+#include <tf2/LinearMath/Quaternion.h>
+#include "tf/transform_listener.h"
+#include <ros/console.h>
+#include <nav_core/base_global_planner.h>
+#include <global_planner/planner_core.h>
 
 using std::string;
 
@@ -19,17 +28,54 @@ bool add(stero_mobile_init::Proj2::Request  &req,
     return true;
 }
 
-void configureCostmap() 
-{   
-    try {
-        tf2_ros::Buffer tf2Buffer;
-        string mapName = "map";
-        costmap_2d::Costmap2DROS costmap(mapName, tf2Buffer);
-    } catch (std::exception& e) {
 
-    }
+// void sendTransform() 
+// {
+//     static tf2_ros::StaticTransformBroadcaster static_broadcaster;
+//     geometry_msgs::TransformStamped tf;
+
+//     tf.header.stamp = ros::Time::now();
+//     tf.header.frame_id = "base_link";
+//     tf.child_frame_id = "odom";
+//     tf.transform.translation.x = 0;
+//     tf.transform.translation.y = 0;
+//     tf.transform.translation.z = 0;
+//     tf2::Quaternion quat;
+//     quat.setRPY(0, 0, 0);
+//     tf.transform.rotation.x = quat.x();
+//     tf.transform.rotation.y = quat.y();
+//     tf.transform.rotation.z = quat.z();
+//     tf.transform.rotation.w = quat.w();
+//     static_broadcaster.sendTransform(tf);
+// }
+
+void configureGlobalCostmap() 
+{   
+    // sendTransform();
+    tf2_ros::Buffer tfBuffer;
+    tf2_ros::TransformListener tfListener(tfBuffer);
+    tfBuffer.setUsingDedicatedThread(true);
+    string mapName = "";
+    costmap_2d::Costmap2DROS costmap("global_costmap", tfBuffer);
+    // ROS_ERROR("xd", "FINISHED");
     
+
+    string globalPlannerName = "base_global_planner";
+    global_planner::GlobalPlanner globalPlanner;
+    globalPlanner.initialize("base_global_planner", &costmap);
+    geometry_msgs::PoseStamped start;
+    start.header.frame_id = "map";
+    geometry_msgs::PoseStamped goal;
+    goal.header.frame_id = "map";
+    goal.pose.position.x = 5;
+    std::vector<geometry_msgs::PoseStamped> plan;
+    bool wasSuccessful = globalPlanner.makePlan(start, goal, plan);
+    char xd[20];
+    std::sprintf(xd, "%d elementow",plan.size());
+    if(wasSuccessful)
+        ROS_ERROR(xd);
 }
+
 
 int main(int argc, char **argv)
 {
@@ -37,7 +83,7 @@ int main(int argc, char **argv)
     ros::NodeHandle n;
 
     ros::ServiceServer service = n.advertiseService("proj2", add);
-    configureCostmap();
+    configureGlobalCostmap();
     ros::spin();
 
     return 0;
